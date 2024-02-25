05d58e86eaac7082cc385af211741f2f0616cb52
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2024-02-24 21:29:59 -0600

    Dictionary of samples

---
+++ sample_files.py
@@ -0,0 +1,33 @@
+
+signal_paths = {
+    "DMScalar_top_tChan_Mchi1_Mphi100": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi100_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi150": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi150_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi200": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi200_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi250": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi250_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi300": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi300_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi350": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi350_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi400": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi400_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi450": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi450_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "DMScalar_top_tChan_Mchi1_Mphi500": "/hdfs/store/user/vshang/tDM_Run2018/DMScalar_top_tChan_Mchi1_Mphi500_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/ModuleCommonSkim_12242022/tree_all.root"
+}
+
+
+
+background_paths = {
+    "TTToSemiLeptonic": "/hdfs/store/user/vshang/ttbarPlusJets_Run2018/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "TTTo2L2Nu": "/hdfs/store/user/vshang/ttbarPlusJets_Run2018/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-100To200": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-100To200_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-200To400": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-200To400_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-400To600": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-400To600_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-600To800": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-600To800_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-800To1200": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-800To1200_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-1200To2500": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-1200To2500_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "ZJetsToNuNu_HT-2500ToInf": "/hdfs/store/user/vshang/ZTo2Nu_Run2018/ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph/ModuleCommonSkim_12242022/tree_all.root",
+    "WJetsToLNu_HT-100To200": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root",
+    "WJetsToLNu_HT-200To400": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root",
+    "WJetsToLNu_HT-400To600": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root",
+    "WJetsToLNu_HT-600To800": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root",
+    "WJetsToLNu_HT-800To1200": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root",
+    "WJetsToLNu_HT-1200To2500": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root",
+    "WJetsToLNu_HT-2500ToInf": "/hdfs/store/user/vshang/WPlusJets_Run2018/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/ModuleCommonSkim_05102023/tree_all.root"
+}
